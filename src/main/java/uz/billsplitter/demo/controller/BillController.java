a85c78af303d6ae09ef6dfb27d4fbb735df7efdd
Mix4Pro <yaelseba@student.21-school.ru>
2025-12-10 16:59:40 +0500

    Added annotations

--- src/main/java/uz/billsplitter/demo/controller/BillController.java
+++ src/main/java/uz/billsplitter/demo/controller/BillController.java
@@ -1,4 +1,7 @@
 package uz.billsplitter.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BillController {
 }
--- src/main/java/uz/billsplitter/demo/service/impl/BillServiceImpl.java
+++ src/main/java/uz/billsplitter/demo/service/impl/BillServiceImpl.java
@@ -1,6 +1,8 @@
 package uz.billsplitter.demo.service.impl;
 
+import org.springframework.stereotype.Service;
 import uz.billsplitter.demo.service.BillService;
 
+@Service
 public class BillServiceImpl implements BillService{
 }
